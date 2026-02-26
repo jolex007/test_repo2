def6d4539979818c63b0bc07dbc739b0ebcdb69f
Alexey <zertsalov.av@phystech.edu>
2026-02-26 13:46:34 +0000

    Removed !

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world!";
+    std::cout << "Hello world";
     std::cout << "Hello Irkutsk!";
     std::cout << "lol kek arg";
     return 0;
